cbdf092f49540cb79a5cc487a978ded5988ce979
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-08-18 18:16:50 +0700

    coba lagi

--- app.js
+++ app.js
@@ -17,28 +17,39 @@ app.get('/', (req, res) => {
   });
 });
 
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
-    .then(results => res.status(200).json(results))
+    .then(result => res.json(result))
     .catch(err => {
-      console.log(err);
+      console.error(err);
       res.status(500).end();
     });
 });
 
+// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  console.log(event);
+  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
-app.listen(process.env.PORT || 4000, function() {
-  console.log('Your node js server is running');
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
