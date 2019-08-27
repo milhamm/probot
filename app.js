4cd3659ac25349f7867d0d1f919402dfd1adb616
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-08-28 01:52:10 +0700

    dialogflow

--- app.js
+++ app.js
@@ -50,24 +50,6 @@ app.post('/dialogflow', (req, res) => {
   };
 
   const contoh = {
-    fulfillmentText: 'This is a text response',
-    fulfillmentMessages: [
-      {
-        card: {
-          title: 'card title',
-          subtitle: 'card text',
-          imageUri:
-            'https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png',
-          buttons: [
-            {
-              text: 'button text',
-              postback: 'https://assistant.google.com/'
-            }
-          ]
-        }
-      }
-    ],
-    source: 'example.com',
     payload: {
       google: {
         expectUserResponse: true,
